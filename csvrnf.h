1333c06c717cb3440fec3aa6af52131d26c1e014
Marios <mariostsaltakis@gmail.com>
2022-04-25 22:04:25 +0300

    learning git

--- csvrnf.h
+++ csvrnf.h
@@ -8,7 +8,7 @@ struct database{     //This struct is our database
 typedef struct database Database;
 extern int csvr(Database *); //This is the csv reader.It creates an array and places the formatted data in it
 extern int daternf (char*); //this function creates an integer out of the date that we can then sort and index easily
-extern void zeromode (int,Database *); // This function finds the mode of the columns and inputs it in the cell with 0s in them
+extern void zeromode (int, Database *); // This function finds the mode of the columns and inputs it in the cell with 0s in them
 extern void print(int , Database*);
 extern void freeData(int , Database*); //This function frees the memory of the database
 
